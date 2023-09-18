33fa1d3ed1b7f03e7855f679d1845dae459bf3d7
i7-8700 <1281261856@qq.com>
2023-09-18 17:59:31 +0800

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,27 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'eslint-config-airbnb',
+    'eslint-config-airbnb/hooks',
+    'eslint-config-airbnb-typescript',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/function-component-definition': 0,
+    'react/react-in-jsx-scope': 0,
+    // 'import/prefer-default-export': 0,
+    'react-hooks/exhaustive-deps': 1,
   },
 }
