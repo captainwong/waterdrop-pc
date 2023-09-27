91753299a1df511836350918955b1b0ff67c3da0
i7-8700 <1281261856@qq.com>
2023-09-27 23:07:48 +0800

    card gql

---
+++ src/graphql/card.ts
@@ -0,0 +1,63 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_OR_UPDATE_CARD = gql`
+  mutation createOrUpdateCard($courseId:String!, $dto: CardInputDto!, $id: String){
+    createOrUpdateCard(courseId:$courseId, dto: $dto, id: $id){
+      code
+      message
+      data{
+        id
+        name
+        type
+        count
+        duration
+      }
+    }
+  }
+`;
+
+export const GET_CARD = gql`
+  query getCardInfo($courseId:String!, $id: String!){
+    getCardInfo(courseId:$courseId, id: $id){
+      code
+      message
+      data{
+        id
+        name
+        type
+        count
+        duration
+        course{
+          id
+          name
+          desc
+        }
+      }
+    }
+  }
+`;
+
+export const GET_CARDS = gql`
+  query getCards($courseId:String!, $name: String){
+    getCards(courseId:$courseId, name: $name){
+      code
+      message
+      data{
+        id
+        name
+        type
+        count
+        duration
+      }
+    }
+  }
+`;
+
+export const DELETE_CARD = gql`
+  mutation deleteCard($courseId:String!, $id: String!){
+    deleteCard(courseId:$courseId, id: $id){
+      code
+      message
+    }
+  }
+`;
